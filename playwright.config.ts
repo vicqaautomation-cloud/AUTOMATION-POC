71643fc9bec328f468458115178c8687f4e6d91d
hugohhernandez76 <hugohhernandez76@gmail.com>
2026-06-04 08:39:18 -0600

    initial playwright POC

---
+++ playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+
+  reporter: [
+    ['line'],
+    ['allure-playwright', { outputFolder: 'allure-results' }],
+  ],
+
+  use: {
+    baseURL: 'https://admin-demo.nopcommerce.com',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
